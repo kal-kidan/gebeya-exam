77cb09a214d38b44846d1384bdeb15a8c43a97ce
kal-kidan <kal05627356@gmail.com>
2020-10-26 08:52:56 -0400

    error format changed in error-handler middleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 const ApiError = require('./../error/ApiError')
 function errorHandler(err, req,res,next){
     if(err instanceof ApiError){
-        return res.status(err.code).json(err.message)
+        return res.status(err.code).json({error: true, message: err.message})
     }
     res.status(500).json({error: true, message: "something went wrong"})
 }
