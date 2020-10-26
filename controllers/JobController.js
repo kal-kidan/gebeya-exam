1f841621dc72dae7eab385e6c3cf8b2c730dc638
kal-kidan <kal05627356@gmail.com>
2020-10-26 07:58:23 -0400

    getJobDetail and getJobList exported

--- controllers/JobController.js
+++ controllers/JobController.js
@@ -14,7 +14,36 @@ const addJob = async (req, res, next)=>{
        }
 }
 
+const getJobDetail = async (req, res, next)=>{
+    try {
+      let {_id} = req.params
+      if(userId!== req.user._id)
+       return res.json({error: true, msg: "you have not the right privilege"})
+       const jobDetail = await Job.findById(_id)
+       if(!jobDetail) return next(ApiError.notFoundError(`job with an id of ${_id} is not found`))
+       else return res.json(jobDetail)
+    } catch (error) {
+        return next(ApiError.internalServerError(error.message))
+    }
+  }
+  const getJobList = async (req, res, next)=>{
+    const limit = parseInt(req.query.limit)  
+    const page = parseInt(req.query.page)
+    if( !(limit > 0 && page > 0) ) {  
+        return next(ApiError.badRequest("invalid input"))
+    }
+    try { 
+        Job.paginate({}, { page, limit }, function(err, result) {
+          return  res.json(result)
+        });
+    } catch (errors) {
+        return next(ApiError.internalServerError(error.message))
+    }
+     
+  }
 
 module.exports = {
-    addJob
+    addJob,
+    getJobDetail,
+    getJobList
 }
